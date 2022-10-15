bdd748eb0485eda219ac020047a16f7dfe605cd1
XeniaX <ksusha.hrisanova@yandex.ru>
2022-10-15 20:34:01 +0300

    program added

---
+++ task1/Program.cs
@@ -0,0 +1,47 @@
+﻿string[] GetArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите строку {i+1}");
+        array[i] = Console.ReadLine();
+    } 
+    return array;   
+}
+
+string[] NMarkArray (int n, string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= n)
+        {
+            count++;
+        }
+    }
+
+    string [] resultArray = new string [count];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= n)
+        {
+            resultArray[j] = array[i];
+            j++;
+        }  
+    }
+    return resultArray;
+}
+
+Console.WriteLine("Введите число строк в массиве:");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array = GetArray(size);
+
+Console.WriteLine();
+
+string[] resultArray = NMarkArray(3, array);
+Console.WriteLine("Получен следующий массив:");
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine(resultArray[i]);
+}
